88aca7fa1c83733e0a22247ae61157727c101b6f
Kaxib-Bygb <90846137+Kaxib-Bygb@users.noreply.github.com>
2021-09-16 20:18:36 +0700

    Update buildrun.sh

--- buildrun.sh
+++ buildrun.sh
@@ -2,7 +2,7 @@
 apt-get update -y
 COUNTER=50
 until [  $COUNTER -lt 10 ]; do
-TOKEN="80c071454f6f995ebee58f87ddd78422a0cda9d8a631489906" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="8709fe4f6165874c426836025b4567db0e29b7d1b00f1487c7" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 timeout 235m ~/.buildkite-agent/bin/buildkite-agent start
 echo "thanks"
 
